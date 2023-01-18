d9051ed0308588c1ff0fe2e0e150c32384e32ff5
ishtiaqSamdani <ishmartishtiaq709@gmail.com>
2023-01-18 18:29:24 +0530

    mass navbar animations

--- components/navbar.js
+++ components/navbar.js
@@ -1,15 +1,37 @@
-import { useRouter } from 'next/router'
-import React from 'react'
+import { useRouter } from "next/router";
+import React from "react";
 
 const Navbar = () => {
-    const router = useRouter()
-    return (
-        <nav className="navbar" style={{ 'backgroundColor': 'rebeccapurple', 'width': '100%', 'padding': '2rem' }}>
-            <button className="btn"
-                onClick={() => { router.push('/login') }}
-            >LOGIN</button>
-        </nav>
-    )
-}
+    const showList = () => {
+        const list = document.querySelector('.list');
+        list.classList.toggle('active');
+    }
+  const router = useRouter();
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="navbar__logo" >
+          <img className="logo" src="./static/nav-bus-logo.svg" alt="logo"/>
+        </div>
 
-export default Navbar
+        <div className="burger-menu">
+          <img src="./static/burger-menu.svg" alt="" onClick={showList}/>
+        </div>
+        <ul className="list">
+          <li
+            className="list__item"
+            onClick={() => {
+              router.push("/login");
+              showList();
+            }}
+          >
+            <div className="login__text">Login</div>
+          </li>
+          <li className="list__item"><div className="login__text">About</div></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
