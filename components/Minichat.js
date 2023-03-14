48edd06a3258071a10122352b48b6e79b999c667
veerraju418 <rajasingamsetti2001@gmail.com>
2023-03-14 20:41:47 +0530

    (global.css,Minichat.js) added profile images in chat area

--- components/Minichat.js
+++ components/Minichat.js
@@ -3,8 +3,14 @@ import React from "react";
 const Minichat = ({ chat, admin }) => {
   return (
     <>
-      <div className={`queryBox ${admin ? "left_chat" : "right_chat"}`}>
-        {chat}
+      <div className={`chatDon ${admin ? "chat_left" : "chat_right"}`}>
+        <div className={` ${admin ? "left_chat" : "right_chat"}`}>
+        <img className="chatImg" src="./static/chatProfile.svg" alt="error"></img>
+        {admin ? (<><p  className="chatText">Admin</p></>) : (<><p className="chatText">User</p></>)}
+        </div>
+        <div className={`queryBox ${admin ? "left_chat" : "right_chat"}`}>
+          {chat}
+        </div>
       </div>
     </>
   );
